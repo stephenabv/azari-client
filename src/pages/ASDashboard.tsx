9fa50fe6e1b133e650d8d87b811bc98fe99d23b0
stephenabv <stephen.abueva@gmail.com>
2026-05-27 19:52:27 +0800

    add todo

--- src/pages/ASDashboard.tsx
+++ src/pages/ASDashboard.tsx
@@ -36,6 +36,8 @@ const DEFAULT_VISIBILITY: SectionVisibility = {
 export default function ASDashboard() {
   const vis = useContent<SectionVisibility>('section-visibility', DEFAULT_VISIBILITY);
 
+  //  TODO: add on/off
+
   return (
     <>
       {vis.hero && (
